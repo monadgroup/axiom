0bb9100e41ca472475e021466d02d4d7102854b0
cpdt <copodt@gmail.com>
2018-01-14 17:08:59 +1000

    added project for compiler

---
+++ compiler/parser/Token.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+
+namespace MaximParser {
+
+    class Token {
+    public:
+        enum class Type {
+
+        };
+
+        Type type;
+        std::string content;
+    };
+
+}
