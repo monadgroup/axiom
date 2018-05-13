79d203acbe66a45e18a651b7ce625d11a692acea
Fire-wood <wingedserpent23+code@gmail.com>
2018-05-13 18:48:34 +1000

    Syntax Highlighter
    Added highlighter to :num syntax

--- editor/widgets/node/SyntaxHighlighter.cpp
+++ editor/widgets/node/SyntaxHighlighter.cpp
@@ -3,9 +3,24 @@
 using namespace AxiomGui;
 
 SyntaxHighlighter::SyntaxHighlighter(QTextDocument *parent) : QSyntaxHighlighter(parent) {
+    HighlightingRule rule;
 
+    keywordFormat.setForeground(Qt::darkCyan);
+    keywordFormat.setFontWeight(QFont::Bold);
+    QStringList keywordPatterns;
+    keywordPatterns << "\\b:num\\b";
+    foreach (const QString &pattern, keywordPatterns) {
+        rule.pattern = QRegularExpression(pattern);
+        rule.format = keywordFormat;
+        highlightingRules.append(rule);
+    }
 }
 
 void SyntaxHighlighter::highlightBlock(const QString &text) {
-
+	foreach (const HighlightingRule &rule, highlightingRules) {
+        QRegularExpressionMatchIterator matchIterator = rule.pattern.globalMatch(text);
+        while (matchIterator.hasNext()) {
+            QRegularExpressionMatch match = matchIterator.next();
+            setFormat(match.capturedStart(), match.capturedLength(), rule.format);
+        }
 }
