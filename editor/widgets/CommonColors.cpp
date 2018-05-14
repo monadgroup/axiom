11bdfd3ce2967a11b3e844692e1f539ebe2e671e
cpdt <copodt@gmail.com>
2018-05-14 22:39:14 +1000

    increase selection brightness

--- editor/widgets/CommonColors.cpp
+++ editor/widgets/CommonColors.cpp
@@ -3,7 +3,7 @@
 using namespace AxiomGui;
 
 const QColor CommonColors::customNodeNormal = QColor(17, 17, 17, 100);
-const QColor CommonColors::customNodeActive = QColor(27, 27, 27, 100);
+const QColor CommonColors::customNodeActive = QColor(37, 37, 37, 100);
 const QColor CommonColors::customNodeBorder = QColor(51, 51, 51);
 
 const QColor CommonColors::errorNodeBorder = QColor(255, 0, 0);
