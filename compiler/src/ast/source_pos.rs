45f8ad7312428fee8781a00c017a586953f35eb5
cpdt <copodt@gmail.com>
2018-06-14 21:13:48 +1000

    make SourcePos debug format a bit nicer to read

--- compiler/src/ast/source_pos.rs
+++ compiler/src/ast/source_pos.rs
@@ -1,12 +1,26 @@
-#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
+use std::fmt;
+
+#[derive(Eq, PartialEq, Clone, Copy, Hash)]
 pub struct SourcePos {
     pub line: usize,
     pub column: usize,
 }
 
+impl fmt::Debug for SourcePos {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.line, self.column)
+    }
+}
+
 pub const UNDEF_SOURCE_POS: SourcePos = SourcePos { line: 0, column: 0 };
 
-#[derive(Debug, Eq, PartialEq, Clone, Copy, Hash)]
+#[derive(Eq, PartialEq, Clone, Copy, Hash)]
 pub struct SourceRange(pub SourcePos, pub SourcePos);
+
+impl fmt::Debug for SourceRange {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?} -> {:?}", self.0, self.1)
+    }
+}
 
 pub const UNDEF_SOURCE_RANGE: SourceRange = SourceRange(UNDEF_SOURCE_POS, UNDEF_SOURCE_POS);
