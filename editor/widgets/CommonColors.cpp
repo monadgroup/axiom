c4ce64b134070c06d8b58dcd4293ce031fe84564
cpdt <copodt@gmail.com>
2018-08-13 00:27:44 +1000

    tweak styling a bit more: lighter headers, more node colour contrast, fixes #40

--- editor/widgets/CommonColors.cpp
+++ editor/widgets/CommonColors.cpp
@@ -2,14 +2,14 @@
 
 using namespace AxiomGui;
 
-const QColor CommonColors::customNodeNormal = QColor(37, 37, 37, 100);
+const QColor CommonColors::customNodeNormal = QColor(27, 27, 27, 100);
 const QColor CommonColors::customNodeActive = QColor(57, 57, 57, 100);
 const QColor CommonColors::customNodeBorder = QColor(51, 51, 51);
 
 const QColor CommonColors::errorNodeBorder = QColor(255, 0, 0);
 
 const QColor CommonColors::groupNodeNormal = QColor(29, 15, 33, 100);
-const QColor CommonColors::groupNodeActive = QColor(58, 30, 66, 100);
+const QColor CommonColors::groupNodeActive = QColor(64, 33, 73, 100);
 const QColor CommonColors::groupNodeBorder = QColor(84, 48, 99);
 
 const QColor CommonColors::numNormal = QColor(52, 152, 219);
