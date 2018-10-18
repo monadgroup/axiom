cfa352c25dc2d7fe9fecf4164234beb773e4f464
cpdt <copodt@gmail.com>
2018-10-18 22:07:57 +1000

    revert previous fix for undeletion as it breaks groups (all these different cases for deletion are starting to get very messy...)

--- editor/model/actions/DeleteObjectAction.cpp
+++ editor/model/actions/DeleteObjectAction.cpp
@@ -65,7 +65,6 @@ void DeleteObjectAction::backward(std::vector<QUuid> &compileItems) {
 
     for (const auto &obj : addedObjects) {
         compileItems.push_back(obj->uuid());
-        compileItems.push_back(obj->parentUuid());
     }
 }
 
