fc48e9a24553c5fa288f0050a61486a3432a1570
cpdt <copodt@gmail.com>
2018-10-28 19:42:43 +1000

    add multiline comment highlighting, fixes #96

--- editor/widgets/node/SyntaxHighlighter.cpp
+++ editor/widgets/node/SyntaxHighlighter.cpp
@@ -1,5 +1,6 @@
 #include "SyntaxHighlighter.h"
 
+#include <QtCore/QRegularExpression>
 #include <QtCore/QStringBuilder>
 
 #include "editor/compiler/interface/FunctionTable.h"
@@ -15,7 +16,10 @@ public:
 
     std::vector<HighlightRule> highlightRules;
 
-    CachedHighlightRules() {
+    QRegularExpression commentExpression;
+    QTextCharFormat commentFormat;
+
+    CachedHighlightRules() : commentExpression(R"((/\*)|(\*/))") {
         // control formatting
         HighlightRule controlRule;
         controlRule.pattern = QRegExp(R"(:(num|num\[\]|midi|midi\[\]|graph|roll|scope)\b)");
@@ -51,11 +55,13 @@ public:
         formRule.format.setFontWeight(QFont::Bold);
         highlightRules.push_back(std::move(formRule));
 
+        commentFormat.setForeground(QColor(121, 121, 121)); // grey
+        commentFormat.setFontWeight(QFont::StyleItalic);
+
         // comment (single line) formatting
         HighlightRule commentRule;
         commentRule.pattern = QRegExp(R"(#[^\n]*)");
-        commentRule.format.setForeground(QColor(121, 121, 121)); // grey
-        commentRule.format.setFontWeight(QFont::StyleItalic);
+        commentRule.format = commentFormat;
         highlightRules.push_back(std::move(commentRule));
     }
 };
@@ -73,4 +79,49 @@ void SyntaxHighlighter::highlightBlock(const QString &text) {
             index = expression.indexIn(text, index + length);
         }
     }
+
+    // Use block state to highlight multiline comments.
+    // Since the language allows multiline comments inside multiline comments (e.g. /* /* */ */), we let the block state
+    // be the depth of the comment at the end of the block. That is, if we're not in a comment the state is 0, otherwise
+    // it's > 0. We increment it whenever we find an opening token, and decrement it when we find a closing one.
+    setCurrentBlockState(0);
+
+    auto enterCount = previousBlockState();
+    if (enterCount == -1) enterCount = 0;
+
+    // Increment/decrement the block state to determine the exit count
+    auto currentPos = 0;
+    auto lastOpenPos = 0;
+    auto exitCount = enterCount;
+    while (currentPos < text.size()) {
+        auto match = cachedRules.commentExpression.match(text, currentPos);
+        if (!match.hasMatch()) break;
+
+        // capture group 1 = comment open
+        // capture group 2 = comment close
+        if (match.capturedLength(1)) {
+            if (exitCount == 0) {
+                lastOpenPos = match.capturedStart();
+            }
+
+            exitCount++;
+        } else if (match.capturedLength(2)) {
+            exitCount--;
+
+            // if we're out of the comment, highlight this part
+            if (exitCount == 0) {
+                setFormat(lastOpenPos, match.capturedEnd() - lastOpenPos, cachedRules.commentFormat);
+            } else if (exitCount < 0)
+                exitCount = 0;
+        }
+
+        currentPos = match.capturedEnd();
+    }
+
+    // if the exit count isn't zero, we're still in a comment, so we need to highlight everything to the end
+    if (exitCount > 0) {
+        setFormat(lastOpenPos, text.size() - lastOpenPos, cachedRules.commentFormat);
+    }
+
+    setCurrentBlockState(exitCount);
 }
